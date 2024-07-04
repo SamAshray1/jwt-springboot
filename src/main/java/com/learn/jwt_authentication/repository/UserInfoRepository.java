039446d3fde4f78cc8929f468344f5c55df17237
SamAshray1 <56475424+SamAshray1@users.noreply.github.com>
2024-07-04 20:31:11 +0530

    first commit

---
+++ src/main/java/com/learn/jwt_authentication/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.learn.jwt_authentication.repository;
+
+import com.learn.jwt_authentication.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+Optional<UserInfo> findByName(String username);
+}
